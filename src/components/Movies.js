47481fc2e4c3f11a7e52608ec9033d0e0d8c43ba
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-20 15:53:25 +0300

    completed lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Movies Page</h1>
+      <div>
+        {movies.map((movie) => {
+          return (
+            <div key={movie.title}>
+              <h2>Name: {movie.title}</h2>
+              <p>Time: {movie.time}</p>
+              <div>
+                <h3>Genres:</h3>
+                <ul>
+                  {movie.genres.map((genre) => {
+                    return <li key={genre}>{genre}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  )
 }
 
 export default Movies;
